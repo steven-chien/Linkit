ac6b59dadc36628fb19c0873e58953847025c75e
Steven Chien <steven.chien@connect.polyu.hk>
2015-05-06 00:47:58 +0800

    Fixed tab activation on nav bars
    - Activation of tabs now depends on the current route returned by Iron Router

--- client/helpers/nav.js
+++ client/helpers/nav.js
@@ -1,35 +1,26 @@
 Template.navItems.helpers({
-	activeIfTemplateIs: function (template) {
-		var currentRoute = Router.current();
-		if(currentRoute) {
-			if(template === currentRoute.lookupTemplate())
-				return 'active';
-			if(template === "Linkitterlist" && currentRoute.url.search("link")>=0)
-				return 'active';
-			return '';
-		}
-	},
-	myProfilePath: function () {
+	myUserId: function () {
 		if(Meteor.userId())
-			return '/profile/'+Meteor.userId();
+			return Meteor.userId();
+	},
+	activateTab: function(templateName) {
+		var currentRoute = Router.current().route.path(this);
+		if(currentRoute.indexOf(templateName.toLowerCase())!=-1)
+			return 'active';
+		else
+			return '';
 	}
 });
 
 Template.list_nav.helpers({
-	activeIfTemplateIs: function (template) {
-		var currentRoute = Router.current();
-		console.log(currentRoute.lookupTemplate());
-		// if(currentRoute)
-		// {
-		// 	if(template === currentRoute.lookupTemplate())
-		// 		return 'active';
-		// 	if(template === "Linkitterlist" && currentRoute.lookupTemplate() === "Addfriend")
-		// 		return 'active';
-		// 	return '';
-		// }
-		return currentRoute && template === currentRoute.lookupTemplate() ? 'active' : '';
-	},
 	addfriend: function() {
 		return "/link/addFriend?id="+LinkitterList.find({username:{ $regex: Meteor.user().profile.name, $options: 'i' }}).fetch()[0]._id;
+	},
+	activateTab: function(templateName) {
+		var currentRoute = Router.current().route.path(this);
+		if(currentRoute.indexOf(templateName.toLowerCase())!=-1)
+			return 'active';
+		else
+			return '';
 	}
 });
