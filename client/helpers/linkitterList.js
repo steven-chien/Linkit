32e368d8544a69f53b435dcc4c5d82cb6de1784a
Steven Chien <steven.chien@connect.polyu.hk>
2015-05-06 00:55:18 +0800

    Added surname into regex matching parameter
    - Not yet implemented substring matching

--- client/helpers/linkitterList.js
+++ client/helpers/linkitterList.js
@@ -2,13 +2,13 @@ Template.linkitterList.helpers({
 	friends: function() { 
 		var userId = Meteor.userId();
 		if(userId) {
-			var searchVal = Session.get('searchBox');
+			var searchVal = Session.get('searchBox').replace(/ /g,'');
 			if(typeof searchVal==='undefined' || searchVal=="" || searchVal==null) {
 				return Profiles.find({ user_id: { $ne: userId } });
 			}
 			else {
 				console.log(searchVal);
-				return Profiles.find({ name: { $regex: searchVal, $options: 'i' }, user_id: { $ne: userId } });
+				return Profiles.find({ $or: [ { name: { $regex: searchVal, $options: 'i' } }, { surname: { $regex: searchVal, $options: 'i' } } ],user_id: { $ne: userId } });
 			}
 		}
 	}
