662d2406ce4c9389c1c68e88f600f8838045a888
Steven Chien <steven.chien@connect.polyu.hk>
2015-05-15 23:53:31 +0800

    Implemented forms and front end for profile edit

--- client/helpers/profile.js
+++ client/helpers/profile.js
@@ -5,5 +5,35 @@ Template.profile.helpers({
 			var profile = Profiles.findOne({ user_id: target_id });
 			return profile;
 		}
+	},
+	isCurrentUser: function() {
+		var userId = Meteor.userId();
+		if(userId) {
+			var targetId = Router.current().data();
+			if(userId==targetId)
+				return true;
+		}
+	},
+	editProfile: function() {
+		var editing = Session.get('editingProfile');
+		if(editing)
+			return true;
+	},
+	editState: function() {
+		var editing = Session.get('editingProfile');
+		if(editing)
+			return 'Done';
+		else
+			return 'Edit Profile';
+	}
+});
+
+Template.profile.events({
+	'click #editProfile': function(evt) {
+		var editingState = Session.get('editingProfile');
+		if(editingState==true)
+			Session.set('editingProfile', false);
+		else
+			Session.set('editingProfile', true);
 	}
 });
